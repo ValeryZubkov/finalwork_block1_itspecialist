9e149c7d2e189ad58449be34ffe723dfe590c9bc
Valery Zubkov <bouddha@mail.ru>
2022-09-17 16:52:11 +0400

    created file and program

--- program/Program.cs
+++ program/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] SelectionArray(string[] arr, int lenWord)
+{
+    string[] resArr = new string[arr.Length];
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= lenWord)
+        {
+            resArr[count] = arr[i];
+            count++;
+        }
+    }
+    Array.Resize(ref resArr, count);
+    return resArr;
+}
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+string[] array = { "2", "hello", "world", "res", ":-)", "1234", "1567", "-2", "Russia", "555" };
+Console.WriteLine("Задан массив мз строковых элементов: ");
+PrintArray(array);
+int lengthWord = 3;
+string[] resArray = SelectionArray(array, lengthWord);
+Console.WriteLine($"Новый массив из строк, длина которых меньше, либо равна {lengthWord} символам: ");
+PrintArray(resArray);
